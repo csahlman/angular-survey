1577be663b1196c217cefabf9db0c17ccc44a7b8
Charlie Sahlman <csahlman@gmail.com>
2014-01-23 09:09:03 -0700

    add angularjs cdn and protect from forgery

--- app/controllers/survey/application_controller.rb
+++ app/controllers/survey/application_controller.rb
@@ -1,4 +1,15 @@
 module Survey
   class ApplicationController < ActionController::Base
+    protect_from_forgery
+    
+    def set_csrf_cookie_for_ng
+      cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+    end
+
+    protected
+
+      def verified_request?
+        super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+      end
   end
 end
